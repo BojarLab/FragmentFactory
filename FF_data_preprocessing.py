83a3dbb75eef472d8f08fa23ed270da3d5cc569b
James Urban <89264215+urbj@users.noreply.github.com>
2025-01-21 12:50:52 +0100

    Updated preprocessing to a fixed bin number

--- FF_data_preprocessing.py
+++ FF_data_preprocessing.py
@@ -6,8 +6,8 @@ FF_data = pd.read_pickle('./FragmentFactory_dataset.pkl')
 max_spectra_values = [max(d.values()) for d in FF_data['peak_d']]
 FF_data['peak_d_norm'] = [{k:v for k,v in (pd.Series(d) / max_spectra_values[i]).items()} for i,d in enumerate(FF_data['peak_d'])]
 FF_data['rounded_mass'] = [np.round(x,0) for x in FF_data['reducing_mass']]
-for mass_value in FF_data['rounded_mass'].unique():
-    new_frames  = np.arange(40,mass_value+5,0.5)
-    FF_data.loc[FF_data['reducing_mass'].round(0) == mass_value,'binned_intensities_norm'] = FF_data.loc[FF_data['reducing_mass'].round(0) == mass_value,'peak_d_norm'].apply(lambda x: bin_intensities(x, new_frames)[0])
+frames = np.arange(40,1860,0.5)
+binned_intensities, mz_remainder = zip(*[bin_intensities(c, frames) for c in FF_data['peak_d_norm']])
+FF_data['binned_intensities_norm'] = binned_intensities
 FF_data = FF_data.drop(columns=['peak_d_norm','rounded_mass'])
 FF_data.to_pickle('./FragmentFactory_dataset_processed.pkl')
